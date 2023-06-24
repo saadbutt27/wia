3452bcf2cabf92532f2a91c0f1d430e3ce879c4f
Saad Nisar Butt <sb03750@gmail.com>
2023-06-24 14:35:55 +0500

    breakpoint changes in form

--- components/contactForm.js
+++ components/contactForm.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function ContactForm() {
   return (
     <>
-      <div className="flex justify-between flex-col xl:flex-row my-10 py-10 px-6 md:px-20">
-        <div className="flex-1 mr-0 xl:mr-10">
-          <h2 className="text-3xl mb-6 font-semibold leading-snug tracking-wider">Contact Form</h2>
+      <div className="flex justify-between flex-col lg:flex-row my-10 py-10 px-6 md:px-20">
+        <div className="flex-1 mr-0 lg:mr-10">
+          <h2 className="text-3xl mb-6 font-semibold leading-snug tracking-wider">
+            Contact Form
+          </h2>
           <form action="">
             <div className="mb-6 shadow-xl">
               <input
@@ -29,13 +31,6 @@ export default function ContactForm() {
                 placeholder="Phone no."
               />
             </div>
-            <div className="mb-6 shadow-xl">
-              <input
-                type="text"
-                className="text-lg border-2 border-slate-300 rounded-md px-3 py-2 w-full"
-                placeholder="Address"
-              />
-            </div>
             <div className="mb-6">
               <textarea
                 className="text-lg border-2 border-slate-300 rounded-md px-3 py-2 w-full shadow-xl"
@@ -54,7 +49,7 @@ export default function ContactForm() {
             </div>
           </form>
         </div>
-        <div className="flex-1 mt-10 xl:mt-0">
+        <div className="flex-1 mt-10 lg:mt-0">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14965.596074763389!2d85.80699244636031!3d20.325128298459642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909070cb7bc8d%3A0x754c83f93687689a!2sChandrasekharpur%2C%20Bhubaneswar%2C%20Odisha%20751016%2C%20India!5e0!3m2!1sen!2s!4v1684522147069!5m2!1sen!2s"
             width="600"
